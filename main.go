8fe19e07adc21e6b83590570be28c8d6c2d1b9f6
supattana-s <supattana.sue@gmail.com>
2023-05-23 11:46:51 +0700

    try import dependencies and using git tag

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/supattana-s/puppy"
 )
 
 func main() {
-	fmt.Println("Hello")
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	s3 := puppy.BigBark()
+	fmt.Println(s3)
+
+	s4 := puppy.BigBarks()
+	fmt.Println(s4)
 }	
